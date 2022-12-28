457a5d2a170d308a9924132ad5b224bb367cbb49
endrcnn <endercan34@gmail.com>
2022-12-29 00:09:58 +0300

    winston logger configuration

--- config/index.js
+++ config/index.js
@@ -19,6 +19,8 @@ module.exports = {
     LANG: process.env.SYSTEM_LANG || "en",
     LOGS: {
         TRANSPORT: process.env.LOG_TRANSPORT || "DB",
-        LOG_LEVEL: process.env.LOG_LEVEL || "info"
+        LOG_LEVEL: process.env.LOG_LEVEL || "info",
+        MAX_FILE_SIZE: process.env.LOG_MAX_FILE_SIZE || "50m", // log file size
+        MAX_FILES: process.env.LOG_MAX_FILES || "14d" // the number of days the logs will be stored
     }
 };
